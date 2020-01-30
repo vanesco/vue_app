1e12b14ac958af2a03329d32ee36ca064d0f4072
vanesco1005 <vanesco1005@gmail.com>
2020-01-30 11:53:47 +0900

    admin 페이지 DB get

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="admin container">
     <div class="contents" :class="$mq">
-
-      <div v-for="(index, item) in boardData" :key="index">
-        {{item}}
-      </div>
-      
+      <ul>
+        <li v-for="(index, item) in boardData" :key="index">
+          <span>
+            <em>id</em>
+            {{item.id}}
+          </span>
+          <span>
+            <em>email</em>
+            {{item.email}}
+          </span>
+          <span>
+            <em>title</em>
+            {{item.title}}
+          </span>
+          <span>
+            <em>message</em>
+            {{item.message}}
+          </span>
+          <span>
+            <em>createdAt</em>
+            {{item.createdAt}}
+          </span>
+        </li>
+      </ul>
     </div>
 
     <main-footer />
@@ -17,7 +36,7 @@ export default {
   name: "admin",
   data() {
     return {
-      boardData : []
+      boardData: []
     };
   },
   mounted() {
@@ -30,7 +49,7 @@ export default {
         this.boardData = response.data;
       })
       .catch(error => {
-        alert("데이터 불러오기 실패");
+        console.log(error);
       });
   },
   methods: {}
@@ -41,14 +60,33 @@ export default {
 .contents {
   width: 70%;
   min-height: 100vh;
-  margin: 10rem auto;
-  background-color: #ddd;
+  margin: 13rem auto;
   &.mobile {
     width: 90%;
     margin: 3rem auto;
   }
   &.tablet {
     width: 90%;
+  }
+  ul {
+    li {
+      padding: 1rem 0;
+      &:first-child {
+        border-top: none;
+      }
+      span {
+        padding: 0.5rem 0;
+        border-bottom: 1px dashed #d6d6d6;
+        display: block;
+        em {
+          width: 8rem;
+          margin: 0 0.5rem 0 0;
+          color: #7d7d7d;
+          border-right: 1px solid #d6d6d6;
+          display: inline-block;
+        }
+      }
+    }
   }
 }
 </style>
